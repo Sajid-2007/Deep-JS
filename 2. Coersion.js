eaf090734ecdf0c732b8baee58d49993d8845bc8
Shaajiddd <mdshajid227@gmail.com>
2025-08-30 20:45:20 +0530

    Coersion

--- 2. Coersion.js
+++ 2. Coersion.js
@@ -8,7 +8,7 @@
 
 // // They are hidden steps the engine follows when executing code.
 
-// // (a) toPrimitive
+// // (a) ToPrimitive
 
 // // it‘s an abstract operation that converts an object - a primitive value (`string`, `number`, `boolean`, etc).
 
@@ -58,7 +58,123 @@
 
 
 
-// (b) toString()
+// (b) ToString()
 
 
+// (c) ToNumber
 
+// (d) ToBoolean
+
+/////////////////////////////////////////////////////////////////////////////
+
+// 2. Cases of Coersion
+
+/////////////////////////////////////////////////////////////////////////////
+
+// 3. Boxing
+
+/////////////////////////////////////////////////////////////////////////////
+
+// 4. Corner Cases of Coersion
+
+/////////////////////////////////////////////////////////////////////////////
+
+// 5. Truthy and Falsy Value
+
+// Falsy Value
+
+// the following 8 values are considered falsy (they become `false`  when converted to boolean) :
+
+// 1. `false`
+// 2. `0`
+// 3. `0`
+// 4. `0n` (BigInt zero)
+// 5. `""` (empty string)
+// 6. `null`
+// 7. `undefined`
+// 8. `NaN`
+
+
+// 1. false
+if(false) {
+  console.log("truthy");
+} else {
+  console.log("Falsy"); // runs
+}
+
+// 2. 0
+if(0) {
+  console.log("truthy");
+} else {
+  console.log("Falsy"); // runs
+}
+
+// 3. -0
+if(-0) {
+  console.log("truthy");
+} else {
+  console.log("Falsy"); // runs
+}
+
+// 4. 0n(BigInt Zero)
+if(0n) {
+  console.log("truthy");
+} else {
+  console.log("Falsy"); // runs
+}
+
+// 5. "" (empty string)
+if("") {
+  console.log("truthy");
+} else {
+  console.log("Falsy"); // runs
+}
+
+// 6. null
+if(null) {
+  console.log("truthy");
+} else {
+  console.log("Falsy"); // runs
+}
+
+// 7. undefined
+if(undefined) {
+  console.log("truthy");
+} else {
+  console.log("Falsy"); // runs
+}
+
+// 8. NaN
+if(NaN) {
+  console.log("truthy");
+} else {
+  console.log("Falsy"); // runs
+}
+
+// Truthy Value
+
+// Everything else (not in the falsy list) is truthy value.
+
+// Examples :
+
+// 1. `"hello"` (non-empty string)
+// 2. `42` (any non-zero number)
+// 3. `[]` (empty array)
+// 4. `{}` (empty object)
+// 5. `function() {}` (functions)
+
+
+// 1. "hello"
+if ("hello") {
+  console.log("This is truthy"); // runs
+}
+
+// 2. []
+if ([]) {
+  console.log("This is truthy"); // runs
+}
+
+// 3. {}
+if ({}) {
+  console.log("This is truthy"); // runs
+} 
