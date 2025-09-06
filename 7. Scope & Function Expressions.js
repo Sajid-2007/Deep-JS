b1af5a10383c84992f6a621b3ac0d9f835bba6f9
Shaajiddd <mdshajid227@gmail.com>
2025-09-06 20:45:35 +0530

    06-09-2025

---
+++ 7. Scope & Function Expressions.js
@@ -0,0 +1,25 @@
+// 1. Function Expressions
+
+function teacher(){
+    console.log("Teacher Fn")
+};
+
+var myTeacher = function anotherTeacher() {  // this is function expression
+    console.log(anotherTeacher);
+};
+
+console.log(teacher); // returns teacher function
+console.log(myTeacher); // returns anotherTeacher function
+console.log(anotherTeacher); //  RefrenceError : anotherTeacher is not defind.
+
+
+// explanation :
+
+// Scope Difference -
+    // teacher - declared globally.
+    // myTeacher - variable holding the function. 
+    // anotherTeacher - visible only inside that function body. 
+
+// Why RefrenceError -
+    //     RefrenceError because there's no variable named "anotherTeacher" in global scope
+
