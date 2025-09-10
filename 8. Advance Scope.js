a7006641899c1b331e11159ae03205ee78a267a9
Shaajiddd <mdshajid227@gmail.com>
2025-09-10 23:45:13 +0530

    10-09-2025

---
+++ 8. Advance Scope.js
@@ -0,0 +1,35 @@
+// // 1. Lexical Scope
+
+// var teacher = "Kyle";
+
+// function otherClass(){
+//   var teacher = "Suzy";
+  
+//   function ask(question){
+//     console.log(teacher, question);
+//   }
+  
+//   ask("Why?");
+// }
+
+// otherClass();
+
+// //Output : "Suzy Why?" Because of Lexical Scope
+
+
+
+// 2. Dynamic Scope
+
+var teacher = "Kyle";
+
+function ask(question){
+  console.log(teacher, question);
+}
+
+function otherClass(){
+  var teacher = "Suzy";
+  ask("Why?")
+}
+
+otherClass(); // If JS Support dynamic scope then will be "Suzy Why?"
+// Actual Output is "Kyle Why?" because JS Lexical Scopr 
