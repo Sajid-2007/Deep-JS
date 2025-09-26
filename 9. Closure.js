5a6a59770e025db2bbb1225b73f8e0cd2bafb755
Shaajiddd <mdshajid227@gmail.com>
2025-09-26 23:57:25 +0530

    26-09-2025

--- 9. Closure.js
+++ 9. Closure.js
@@ -3,3 +3,14 @@
 // Closure is when a function "remembers" its lexical
 // scope even when the function is executed
 // outside that lexical scope.
+
+function ask(question){
+    console.log("Waitt Buddy")
+    setTimeout(function waitASec(){
+        console.log(question);
+    }, 1000);
+}
+
+ask("What is closure?");
+
+
